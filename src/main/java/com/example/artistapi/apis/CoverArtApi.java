5facbad6ac6ada7b49376af20e7e244bc12657c4
markushhhhhh <markus.haggblad@gmail.com>
2021-12-15 10:22:24 +0100

    Add files via upload

--- src/main/java/com/example/artistapi/apis/CoverArtApi.java
+++ src/main/java/com/example/artistapi/apis/CoverArtApi.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.web.reactive.function.client.WebClient;
 
-import io.netty.handler.timeout.TimeoutException;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
@@ -38,7 +37,7 @@ public class CoverArtApi {
     public String callCoverArt(String id) {
         LOGGER.info("Calling CAA API");
         String caData;
-        try {
+
         caData = webClient.get()
                 .uri(caEndpoint, id)
                 .accept(MediaType.APPLICATION_JSON)
@@ -46,21 +45,9 @@ public class CoverArtApi {
                 .onStatus(status -> status.equals(HttpStatus.NOT_FOUND),
                         response -> Mono.error(new NoCoverArtException("No cover art found for album: " + id)))
                 .bodyToMono(String.class)
-                .timeout(Duration.ofSeconds(4))
+                .timeout(Duration.ofSeconds(3))
                 .retry(2)
                 .block();
-        }
-        catch (NoCoverArtException e) {
-            LOGGER.info(e.getMessage());
-            return "http://no-coverart-available-at-source.se";
-        } catch (TimeoutException e) {
-            // IMPROVEMENT better solution
-            LOGGER.info(e.getMessage());
-            return "http://timeout.se";
-        } catch (Exception e) {
-            LOGGER.info(e.getMessage());
-            return null;
-        }
 
         return caData;
     }
